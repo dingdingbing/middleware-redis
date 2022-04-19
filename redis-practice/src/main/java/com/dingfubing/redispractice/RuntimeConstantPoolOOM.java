0ee418d79bea7d8f708030d8c0ad9df202893b47
dingfubing <1165954612@qq.com>
2022-04-19 14:39:41 +0800

    feat:java string 1.7 和 1.8的区别，常量池再1.8移到了堆中。所以运行是true，如果是1.7，则是false

--- redis-practice/src/main/java/com/dingfubing/redispractice/RuntimeConstantPoolOOM.java
+++ redis-practice/src/main/java/com/dingfubing/redispractice/RuntimeConstantPoolOOM.java
@@ -12,11 +12,14 @@ import java.util.Set;
 public class RuntimeConstantPoolOOM {
 
     public static void main(String[] args) {
-        Set<String> set = new HashSet<>();
-        short i = 0;
-        while (true) {
-            set.add(String.valueOf(i++).intern());
-        }
+        // Set<String> set = new HashSet<>();
+        // short i = 0;
+        // while (true) {
+        //     set.add(String.valueOf(i++).intern());
+        // }
+        String s = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(s.intern() == s);
+        System.out.println("计算机".intern());
     }
 
 }

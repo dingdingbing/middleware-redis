fa829c06e1d2c850ed1333b77f9c6e4d6079670d
dingfubing <1165954612@qq.com>
2023-02-27 18:52:32 +0800

    feat:add somethings

--- redis-practice/src/main/java/com/dingfubing/algorithm/EasyLeetcode28.java
+++ redis-practice/src/main/java/com/dingfubing/algorithm/EasyLeetcode28.java
@@ -9,10 +9,25 @@ package com.dingfubing.algorithm;
 public class EasyLeetcode28 {
 
     public static void main(String[] args) {
-
+        System.out.println(strStr("sadbutsad", "sad"));
     }
 
-    public int strStr(String haystack, String needle) {
+    public static int strStr(String haystack, String needle) {
+        // 双指针
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int temp = i;
+            int j = 0;
+            while (temp < haystack.length() && j < needle.length() && haystack.charAt(temp) == needle.charAt(j)) {
+                temp++;j++;
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
         return -1;
     }
 
